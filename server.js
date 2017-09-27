b93857bc20c9036ec1751c6929aa5731764067be
toneloke <guerrerotonyjs@gmail.com>
2017-09-27 15:04:30 -0400

    html added

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ const server2 = http.createServer((req, res) => {
 })
 server2.listen(1738)
 // build out a server that renders html
-
+const codeIm = require('./codeimmersive')
+const server3 = http.createServer((req, res) => {
+  console.log('connected to server 3 something else')
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write(codeIm)
+  res.end()
+})
+server3.listen(2222)
 // check the request object for the url
 // use 3rd party node modules
 // based on url render different html files
