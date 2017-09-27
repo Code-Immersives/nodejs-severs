c86e0ebcdb09418387611abd4423afbf4217ae2b
toneloke <guerrerotonyjs@gmail.com>
2017-09-27 14:10:22 -0400

    added morgan

---
+++ server.js
@@ -0,0 +1,8 @@
+// build out a server that renders plain text
+
+// build out a server that renders json
+// build out a server that renders html
+// check the request object for the url
+// use 3rd party node modules
+// based on url render different html files
+// maybe get into params
